c29b4ad52ae592b394803f9de81a8865f174e5df
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2023-08-09 19:52:57 -0300

    create components

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import style from './'
+
+export default function Sidebar() {
+  return (
+    <aside className={style.sidebar}>
+      <ul>
+        <li>Section 1</li>
+        <li>Section 2</li>
+        <li>Section 3</li>
+        <li>Section 4</li>
+        <li>Section 5</li>
+        <li>Section 6</li>
+        <li>Section 7</li>
+      </ul>
+    </aside>
+  )
+}
